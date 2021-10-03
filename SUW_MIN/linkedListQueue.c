0e4fd00805b5081f0f7af2608a693205d54c4258
kimminji940724@gmail.com <kimminji940724@gmail.com>
2021-10-04 03:12:24 +0900

    linkedListQueue

--- SUW_MIN/linkedListQueue.c
+++ SUW_MIN/linkedListQueue.c
@@ -1,76 +1,86 @@
+//큐 - 연결리스트 이용
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
+#include <string.h>
 
-typedef int element;
-
-typedef struct {
-	element data;
-	struct StackNode* link;
-} StackNode;
-
-typedef struct {
-	StackNode* top;
-} LinkedStackType;
-
-// 초기화 함수
-void init(LinkedStackType* s)
+typedef struct Node //노드 정의
 {
-	s->top = NULL;
-}
-// 공백 검출
-int is_empty(LinkedStackType* s) {
-	return (s->top == NULL);
-}
-// 포화 검출
-int is_full(LinkedStackType* s) {
-	return 0;
-}
-// 삽입
-void push(LinkedStackType* s, element item) {
-	StackNode* temp =
-		(StackNode*)malloc(sizeof(StackNode));
-	temp->data = item;
-	temp->link = s->top;
-	s->top = temp;
-}
-// 삭제
-element pop(LinkedStackType* s)
+    int data;
+    struct Node *next;
+}Node;
+
+
+typedef struct Queue //Queue 구조체 정의
 {
-	if (is_empty(s)) {
-		fprintf(stderr, "스택이 비어있음\n");
-		exit(1);
-	}
-	else {
-		StackNode* temp = s->top;
-		element data = temp->data;
-		s->top = s->top->link;
-		free(temp);
-		return data;
-	}
-}
-// 출력
-void print_stack(LinkedStackType* s) {
-	for (StackNode* p = s->top; p != NULL; p = p->link)
-		printf("%d->", p->data);
-	printf("NULL \n");
-}
+    Node *front; //맨 앞(꺼낼 위치)
+    Node *rear; //맨 뒤(보관할 위치)
+    int count;//보관 개수
+}Queue;
+
+void InitQueue(Queue *queue);//큐 초기화
+int IsEmpty(Queue *queue); //큐가 비었는지 확인
+void Enqueue(Queue *queue, int data); //큐에 보관
+int Dequeue(Queue *queue); //큐에서 꺼냄
+
 int main(void)
 {
-	LinkedStackType s;
-	init(&s);
-	push(&s, 1); print_stack(&s);
-	push(&s, 2); print_stack(&s);
-	push(&s, 3); print_stack(&s);
-	pop(&s); print_stack(&s);
-	pop(&s); print_stack(&s);
-	pop(&s); print_stack(&s);
-	return 0;
+    int i;
+    Queue queue;
+
+    InitQueue(&queue);//큐 초기화
+    for (i = 1; i <= 5; i++)//1~5까지 큐에 보관
+    {
+        Enqueue(&queue, i);
+    }
+    while (!IsEmpty(&queue))//큐가 비어있지 않다면 반복
+    {
+        printf("%d ", Dequeue(&queue));//큐에서 꺼내온 값 출력
+    }
+    printf("\n");
+    return 0;
 }
-/* 실행결과
-1->NULL
-2->1->NULL
-3->2->1->NULL
-2->1->NULL
-1->NULL
-NULL
-*/
+
+void InitQueue(Queue *queue)
+{
+    queue->front = queue->rear = NULL; //front와 rear를 NULL로 설정
+    queue->count = 0;//보관 개수를 0으로 설정
+}
+
+int IsEmpty(Queue *queue)
+{
+    return queue->count == 0;    //보관 개수가 0이면 빈 상태
+}
+
+void Enqueue(Queue *queue, int data)
+{
+    Node *now = (Node *)malloc(sizeof(Node)); //노드 생성
+    now->data = data;//데이터 설정
+    now->next = NULL;
+
+    if (IsEmpty(queue))//큐가 비어있을 때
+    {
+        queue->front = now;//맨 앞을 now로 설정       
+    }
+    else//비어있지 않을 때
+    {
+        queue->rear->next = now;//맨 뒤의 다음을 now로 설정
+    }
+    queue->rear = now;//맨 뒤를 now로 설정   
+    queue->count++;//보관 개수를 1 증가
+}
+
+int Dequeue(Queue *queue)
+{
+    int re = 0;
+    Node *now;
+    if (IsEmpty(queue))//큐가 비었을 때
+    {
+        return re;
+    }
+    now = queue->front;//맨 앞의 노드를 now에 기억
+    re = now->data;//반환할 값은 now의 data로 설정
+    queue->front = now->next;//맨 앞은 now의 다음 노드로 설정
+    free(now);//now 소멸
+    queue->count--;//보관 개수를 1 감소
+    return re;
+}
